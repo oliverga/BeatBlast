cc02de6c2d77c4c72858b650ce21268b0a3896b9
oliverga <hi@oliverga.com>
2023-02-16 22:19:02 +0100

    justerede "samme genre" sektion

--- albumside.js
+++ albumside.js
@@ -7,46 +7,48 @@ const options = {
 }
 
 const openUrl = new URL(window.location.href);
-const id = openUrl.searchParams.get("id");
-
+let id = openUrl.searchParams.get("id");
 
 const tempUrl = "/temp.json";
-
 let coverurl;
-
 let spotifyId;
-
 const template = document.querySelector("#similar-album-template");
 
 const colorThief = new ColorThief();
 
 async function getJson() {
     // fetch data from restdb
-    const response = await fetch(tempUrl);
+    const response = await fetch(apiUrl, options);
     const data = await response.json();
     console.log(data)
     addToDom(data);
-    // analyzeImageColor();
 }
 
 
-
+// add data to DOM
 function addToDom(data) {
+  // loop through data
     data.forEach(album => {
       // if _id == id then show album on page
       if (album._id == id) {
-        coverurl = "tempimgs/" + album.billede;
-        console.log(coverurl)
+        // split album.billede into just filename and add to coverurl
+        coverurl = "tempimgs/" + album.billede.split(".")[0] + ".jpg";
+
+        // set coverImg src to coverurl for colorThief
         coverImg.src = coverurl;
+
         document.querySelector(".album-cover").src = coverurl;
         document.querySelector(".album-titel").textContent = album.album;
         document.querySelector(".album-artist").textContent = album.artist;
-        // document.querySelector(".spotify-link").href = album.spotifylink;
-        console.log(album)
-        // split album.spotifylink into just spotifyId
+
+        // split album.spotifylink into just spotifyId and add to iframe src
         spotifyId = album.spotifylink.split("album/")[1];
         document.querySelector(".spotify-embed").src = "https://open.spotify.com/embed/album/" + spotifyId + "?utm_source=generator&theme=0";
+
+        // after 200ms set album container div opacity to 1 and rotate album-container smoothly wauw
         setTimeout(() => {
+          // document.querySelector(".vinyl-record").classList.remove("hide");
+          document.querySelector(".add-to-collection").classList.remove("hide");
           document.querySelector(".album-container").style.opacity = "1";
           // 3dtransform album-container to rotate a bit
           document.querySelector(".albumcover-container").style.transform = "rotate3d(0, -1, 0, 12deg)";
@@ -54,10 +56,12 @@ function addToDom(data) {
           document.querySelector(".vinyl-record").style.transform = "translate(0px)"; 
         }, 200);
       }
+
+      // tilføj logik til at tjekke om album er i samme genre som det album der er åbnet
       else {
         // clone template and add to .similar-albums
         const clone = template.content.cloneNode(true);
-        clone.querySelector("img").src = "tempimgs/" + album.billede;
+        clone.querySelector("img").src = "tempimgs/" + album.billede.split(".")[0] + ".jpg";
         clone.querySelector("a").href = "albumside.html?id=" + album._id;
         document.querySelector(".similar-albums-list").appendChild(clone);
       }
@@ -71,17 +75,17 @@ coverImg.onload = function() {
   const palette = colorThief.getPalette(coverImg, 5);
   console.log(palette);  
 
+  // for each color in palette, set css variable to that color
+  palette.forEach((color, index) => {
+    document.documentElement.style.setProperty(`--color${index + 1}`, `rgb(${color[0]}, ${color[1]}, ${color[2]})`);
+  });
 
-  document.documentElement.style.setProperty('--color1', `rgb(${palette[0][0]}, ${palette[0][1]}, ${palette[0][2]})`);
-  document.documentElement.style.setProperty('--color2', `rgb(${palette[1][0]}, ${palette[1][1]}, ${palette[1][2]})`);
-  document.documentElement.style.setProperty('--color3', `rgb(${palette[2][0]}, ${palette[2][1]}, ${palette[2][2]})`);
-  document.documentElement.style.setProperty('--color4', `rgb(${palette[3][0]}, ${palette[3][1]}, ${palette[3][2]})`);
-  document.documentElement.style.setProperty('--color5', `rgb(${palette[4][0]}, ${palette[4][1]}, ${palette[4][2]})`);
-
-  document.querySelector(".bg").style.background = `linear-gradient(135deg, var(--color2) -50%, rgb(0, 0, 0) 120%)`;
-
+  // add gradient to background and set opacity to 1
+  document.querySelector(".bg").style.background = `linear-gradient(135deg, var(--color1) -50%, rgb(0, 0, 0) 120%)`;
   document.querySelector(".bg").style.opacity = "1";
   
+  // give .vinyl-center a background radial gradient with 2 of the colors from the palette
+  document.querySelector(".vinyl-center").style.background = `radial-gradient(circle at 50% 50%, var(--color2) 0%, var(--color3) 100%)`;
 
 };
 
