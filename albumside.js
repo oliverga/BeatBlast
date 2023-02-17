e187c636ee8e0f8acc98c64e25ce6608c3fde701
oliverga <hi@oliverga.com>
2023-02-17 13:22:16 +0100

    albumside - genrefilter, mobile vers, album data

--- albumside.js
+++ albumside.js
@@ -6,38 +6,74 @@ const options = {
     }
 }
 
+const tempUrl = "/temp.json";
+
 const openUrl = new URL(window.location.href);
+
 let id = openUrl.searchParams.get("id");
 
-const tempUrl = "/temp.json";
 let coverurl;
 let spotifyId;
-const template = document.querySelector("#similar-album-template");
+let albumGenre;
+
+const template = document.querySelector("#genre-album-template");
 
 const colorThief = new ColorThief();
 
-async function getJson() {
+
+
+async function getData() {
     // fetch data from restdb
     const response = await fetch(tempUrl);
     const data = await response.json();
     console.log(data)
+    // call function to add data to DOM
     addToDom(data);
 }
 
+async function getGenreAlbums() {
+  // fetch data from restdb
+  const response = await fetch(tempUrl);
+  const data = await response.json();
+  console.log(data)
+  // call function to add data to DOM
+  addGenreAlbums(data);
+}
 
-// add data to DOM
-function addToDom(data) {
+function addGenreAlbums(data) {
   // loop through data
+  data.forEach(album => {
+    // if album.genre == albumGenre && album._id != id then show album on page
+
+    if (album.genre == albumGenre && album._id != id) {
+      console.log(album.genre)
+      // split album.billede into just filename and add to coverurl
+      let genreCoverUrl = "tempimgs/" + album.billede.split(".")[0] + ".jpg";
+      // clone template and add to .similar-albums
+      const clone = template.content.cloneNode(true);
+      clone.querySelector("img").src = genreCoverUrl;
+      clone.querySelector("a").href = "albumside.html?id=" + album._id;
+      document.querySelector(".genre-albums-list").appendChild(clone);
+      return;
+    }
+  })
+}
+
+
+function addToDom(data) {
+  // loop through data //timeout is temporary in order to simulate loading time
   setTimeout(() => {
     data.forEach(album => {
       // if _id == id then show album on page
       if (album._id == id) {
+        albumGenre = album.genre;
         // split album.billede into just filename and add to coverurl
         coverurl = "tempimgs/" + album.billede.split(".")[0] + ".jpg";
 
         // set coverImg src to coverurl for colorThief
         coverImg.src = coverurl;
 
+        // set album data to DOM
         document.querySelector(".album-cover").src = coverurl;
         document.querySelector(".album-titel").textContent = album.album;
         document.querySelector(".album-artist").textContent = album.artist;
@@ -45,40 +81,50 @@ function addToDom(data) {
         // split album.spotifylink into just spotifyId and add to iframe src
         spotifyId = album.spotifylink.split("album/")[1];
         document.querySelector(".spotify-embed").src = "https://open.spotify.com/embed/album/" + spotifyId + "?utm_source=generator&theme=0";
+        document.querySelector(".spotify-link").href = "https://open.spotify.com/album/" + spotifyId + "?utm_source=generator&theme=0";
 
-        // after 200ms set album container div opacity to 1 and rotate album-container smoothly wauw
+        // clone template and add to .album-info
+        const infoClone = document.querySelector("#info-template").content.cloneNode(true);
+        let date = album.dato.split("T")[0];
+        infoClone.querySelector(".info-dato").textContent = date;
+        infoClone.querySelector(".info-label").textContent = album.label;
+        infoClone.querySelector(".info-genre").textContent = album.genre;
+        infoClone.querySelector(".info-sprog").textContent = album.sprog;
+        // append
+        document.querySelector(".album-info").appendChild(infoClone);
+
+        // after a bit set album container div opacity to 1 and rotate album-container smoothly wauw
         setTimeout(() => {
-          // document.querySelector(".vinyl-record").classList.remove("hide");
+          document.querySelector(".loader").classList.add("hide");
           document.querySelector(".add-to-collection").classList.remove("hide");
           document.querySelector(".vinyl-record").classList.remove("hide");
-          document.querySelector(".loader").classList.add("hide");
           document.querySelector(".album-container").style.opacity = "1";
-          document.querySelector(".spotify-embed").style.opacity = "1";
-          // 3dtransform album-container to rotate a bit
+          document.querySelector(".spotify-section").style.opacity = "1";
+          document.querySelector(".album-info").style.opacity = "1";
+          document.querySelector(".album-info").style.transform = "translate(0px)";
+
+          document.querySelector(".genre-albums-section").style.opacity = "1";
+          
+          // 3dtransform album-container to rotate and scale a bit
           document.querySelector(".albumcover-container").style.transform = "rotate3d(0, 1, 0, 6deg)";
           document.querySelector(".albumcover-container").style.scale = "1.07";
           document.querySelector(".vinyl-record").style.transform = "translate(0px)"; 
         }, 800);
-      }
-
-      // tilføj logik til at tjekke om album er i samme genre som det album der er åbnet
-      else {
-        // clone template and add to .similar-albums
-        const clone = template.content.cloneNode(true);
-        clone.querySelector("img").src = "tempimgs/" + album.billede.split(".")[0] + ".jpg";
-        clone.querySelector("a").href = "albumside.html?id=" + album._id;
-        document.querySelector(".similar-albums-list").appendChild(clone);
+        // call function to get similar albums with genre and id as parameters
+        getGenreAlbums();
       }
     })
   }, 900);
+  // call function to get similar albums
+
 }
+
 
 const coverImg = new Image();
 coverImg.onload = function() {
   // Image is fully loaded, now create a ColorThief instance and get the color palette
   const colorThief = new ColorThief();
-  const palette = colorThief.getPalette(coverImg, 5);
-  console.log(palette);  
+  const palette = colorThief.getPalette(coverImg, 10);
 
   // for each color in palette, set css variable to that color
   palette.forEach((color, index) => {
@@ -94,4 +140,6 @@ coverImg.onload = function() {
 
 };
 
-getJson();
+window.onload = (event) => {
+  getData();
+}
