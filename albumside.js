061b85edd729d12e62aaf15271d05ad1687d289b
oliverga <hi@oliverga.com>
2023-02-18 13:03:51 +0100

    added localdata storage

--- albumside.js
+++ albumside.js
@@ -1,52 +1,56 @@
-// declare rest api url and api key
+// declare variables
 const apiUrl = "https://albumliste-d3cb.restdb.io/rest/album";
 const options = {
     headers: {
         'x-apikey': '63eb5946478852088da68231'
     }
 }
-
 const tempUrl = "/temp.json";
-
 const openUrl = new URL(window.location.href);
-
 let id = openUrl.searchParams.get("id");
-
 let coverurl;
 let spotifyId;
 let albumGenre;
-
 const template = document.querySelector("#genre-album-template");
+const localData = JSON.parse(localStorage.getItem("albumData"));
 
-const colorThief = new ColorThief();
-
-
-async function getData() {
-    // fetch data from restdb
-    const response = await fetch(apiUrl, options);
-    const data = await response.json();
-    console.log(data)
-    // call function to add data to DOM
-    addToDom(data);
-}
-
-async function getGenreAlbums() {
-  // fetch data from restdb
+async function fetchData() {
   const response = await fetch(apiUrl, options);
   const data = await response.json();
-  console.log(data)
-  // call function to add data to DOM
-  addGenreAlbums(data);
+  // if data is different from localData then update localData
+  if (!localData || JSON.stringify(localData) !== JSON.stringify(data)) {
+    localStorage.setItem("albumData", JSON.stringify(data));
+    console.log("Data updated in local storage");
+  }
+
+  return data;
+}
+
+async function checkLocalStorage() {
+  // check if data is in local storage
+  if (localData) {
+    console.log("Data retrieved from local storage");
+    // if so, add data to DOM
+    addToDom(localData);
+  }
+  else {
+    // if not, fetch data from API
+    const latestData = await fetchData();
+    console.log("Data retrieved from API");
+    // update local storage with new data
+    localStorage.setItem("albumData", JSON.stringify(latestData));
+    console.log("Data updated in local storage");
+    // add data to DOM
+    addToDom(latestData);
+  }
 }
 
 function addGenreAlbums(data) {
   // loop through data
   data.forEach(album => {
     // if album.genre == albumGenre && album._id != id then show album on page
-
     if (album.genre == albumGenre && album._id != id) {
-      console.log(album.genre)
-      // split album.billede into just filename and add to coverurl
+      // split album.billede into just filename
       let genreCoverUrl = "tempimgs/" + album.billede.split(".")[0] + ".jpg";
       // clone template and add to .similar-albums
       const clone = template.content.cloneNode(true);
@@ -58,10 +62,9 @@ function addGenreAlbums(data) {
   })
 }
 
-
 function addToDom(data) {
-  // loop through data //timeout is temporary in order to simulate loading time
-  // setTimeout(() => {
+  // loop through data // timeout is temporary until animations are done
+  setTimeout(() => {
     data.forEach(album => {
       // if _id == id then show album on page
       if (album._id == id) {
@@ -92,7 +95,7 @@ function addToDom(data) {
         // append
         document.querySelector(".album-info").appendChild(infoClone);
 
-        // after a bit set album container div opacity to 1 and rotate album-container smoothly wauw
+        // data animations and stuff, going to fix this later using css animations
         setTimeout(() => {
           document.querySelector(".loader").classList.add("hide");
           document.querySelector(".add-to-collection").classList.remove("hide");
@@ -101,24 +104,21 @@ function addToDom(data) {
           document.querySelector(".spotify-section").style.opacity = "1";
           document.querySelector(".album-info").style.opacity = "1";
           document.querySelector(".album-info").style.transform = "translate(0px)";
-
           document.querySelector(".genre-albums-section").style.opacity = "1";
           
           // 3dtransform album-container to rotate and scale a bit
           document.querySelector(".albumcover-container").style.transform = "rotate3d(0, 1, 0, 6deg)";
           document.querySelector(".albumcover-container").style.scale = "1.07";
           document.querySelector(".vinyl-record").style.transform = "translate(0px)"; 
-        }, 100);
-        // call function to get similar albums with genre and id as parameters
-        getGenreAlbums();
+        }, 500);
+        // return data so it can be used in other functions
+        addGenreAlbums(data);
       }
     })
-  // }, 870);
-  // call function to get similar albums
-
+  }, 900);
 }
 
-
+// ColorThief stuff
 const coverImg = new Image();
 coverImg.onload = function() {
   // Image is fully loaded, now create a ColorThief instance and get the color palette
@@ -135,10 +135,13 @@ coverImg.onload = function() {
   document.querySelector(".bg").style.opacity = "1";
   
   // give .vinyl-center a background radial gradient with 2 of the colors from the palette
-  document.querySelector(".vinyl-center").style.background = `radial-gradient(circle at 50% 50%, var(--color2) 0%, var(--color3) 100%)`;
+  document.querySelector(".vinyl-center").style.background = `radial-gradient(circle at 50% 50%, var(--color4) 0%, var(--color2) 100%)`;
 
 };
 
+// when page is loaded, call checkLocalStorage
 window.onload = (event) => {
-  getData();
+  checkLocalStorage();
 }
+
+setInterval(fetchData, 60 * 60 * 1000); // fetch new data every hour
